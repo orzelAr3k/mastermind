aa606412cd58eec53832a50bf115b4b4a2db4b84
orzelAr3k <63066341+orzelAr3k@users.noreply.github.com>
2020-05-16 20:20:23 +0200

    Zapis gry do pliku

---
+++ save_game.py
@@ -0,0 +1,25 @@
+def save_game():
+    '''Zapisanie gry'''
+    global pegs
+    global colours 
+    global gameAnswer
+    global gameBoard
+    global tries
+    global colourChoice2
+    global whiteBoard 
+    global blackBoard
+    
+    f1 = open('savegame.txt','w')
+    f1.write("%s\n" % pegs)
+    f1.write("%s\n" % colours)
+    f1.write(' ')
+    for i in gameAnswer:
+        f1.write("%s," % i)
+    f1.write('\n')
+    f1.write("%s\n" % gameBoard)
+    f1.write("%s\n" % tries)
+    f1.write("%s\n" % colourChoice2)
+    f1.write("%s\n" % whiteBoard)
+    f1.write("%s\n" % blackBoard)
+    f1.close()
+    sys.exit()
